42118de17269f7edf6d8c6d2a8ca957bee73f86e
landec <109702291+landec@users.noreply.github.com>
2026-08-05 16:10:47 -0400

    Complete 06-repeatString exercise

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(text, numOfTimes) {
+    let finalString = "";
+    if (numOfTimes < 0) { return 'ERROR' }
+    for (let i = 0; i < numOfTimes; i++) {
+         finalString += text;
+    }
+    return finalString;
 };
 
 // Do not edit below this line
