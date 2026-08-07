f84fe073bef2b9efe0babe988b6a14ffef7e282b
landec <109702291+landec@users.noreply.github.com>
2026-08-07 11:41:51 -0400

    Complete 08-removeFromArray exercise

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...theArgs) {
+    let newArr = [];
+
+    for (let num of arr) {
+        if (!(theArgs.includes(num)) && !(newArr.includes(num)))
+            newArr.push(num);
+    }
+
+    return newArr;
 };
 
 // Do not edit below this line
