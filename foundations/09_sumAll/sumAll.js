37df80fe9f392c0312997ab3270e9afb9cc9e5e0
landec <109702291+landec@users.noreply.github.com>
2026-08-07 18:19:30 -0400

    Complete 09-sumAll exercise

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
+const sumAll = function(firstInt, secondInt) {
+    const maxValue = Math.max(firstInt, secondInt);
+    const minValue = Math.min(firstInt, secondInt);
+    let result = 0;
 
+    if ((firstInt > 0 && secondInt > 0) && (Number.isInteger(firstInt) && Number.isInteger(secondInt))) {
+        for (let i = minValue; i <= maxValue; i++) {
+            result += i;
+        }    
+    } else { return 'ERROR' }
+    
+     return result;
 };
 
 // Do not edit below this line
